3df54fb3f365e5003a9a01b2924715ad8324beed
Ashton Regensberg <aregensberg@gmail.com>
2020-05-05 15:59:10 -0600

    sql

--- sql/.sql
+++ sql/.sql
@@ -1,4 +1,4 @@
-
+ALTER DATABASE aregensberg CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 DROP TABLE IF EXISTS highlight;
 DROP TABLE IF EXISTS item;
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile(
 	profileId binary (16)not null,
-	profileActivationToken char(32),
+	profileActivationToken char(32) not null,
 	profileEmail varchar (128) not null,
 	profileHash char(64) not null,
 	profileImage varchar(255),
@@ -18,7 +18,7 @@ CREATE TABLE profile(
 CREATE TABLE story(
 	storyId binary(16) not null,
 	storyProfileId binary (16) not null,
-	storyContent char(200),
+	storyContent char(200) not null ,
 	storyDate date(3) not null,
 	index(storyProfileId),
 	foreign key(storyProfileId) references profile(profileId),
@@ -30,7 +30,7 @@ CREATE TABLE item(
 	itemStoryId binary(16) not null,
 	itemImage varchar(255),
 	itemDate date(3) not null,
-	itemPrice decimal(3,2) not null,
+	itemPrice decimal(5,2) not null,
 	index(itemStoryId),
 	foreign key(itemStoryId) references story(storyId),
 	primary key(itemId)
@@ -43,4 +43,5 @@ CREATE TABLE highlight(
 	index(highlightStoryId),
 	foreign key(highlightProfileId) references profile(profileId),
 	foreign key(highlightStoryId) references story(storyId)
+	primary key(highlightProfileId, highlightStoryId)
 );
